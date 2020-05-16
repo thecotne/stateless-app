13f3d9b9a384f148f50746950c4de024764b169e
Cotne Nazarashvili <cotne.95@gmail.com>
2020-05-17 02:21:07 +0400

    update some files

--- workspaces/frontend/helpers/hooks.js
+++ workspaces/frontend/helpers/hooks.js
@@ -1,16 +1,16 @@
 // @flow strict-local
 import * as React from 'react'
 
-export function useDidMount (fn: () => void | (() => void)) {
+export function useDidMount (fn: () => undefined | (() => undefined)) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(fn, [])
 }
 
-type ResetableFormFieldProps<Value> = {|
-  +value: Value,
-  +update: Value => mixed,
-  +reset: void => mixed
-|}
+type ResetableFormFieldProps<Value> = {
+  value: Value,
+  update: Value => mixed,
+  reset: () => mixed
+}
 
 export function useField<T> (defaultValue: T): ResetableFormFieldProps<T> {
   const [value, update] = React.useState<T>(defaultValue)
